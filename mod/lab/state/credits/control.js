30b9c8b870acfde441f1446c4b213a42bb6247d9
Igor Khotin <khotin@gmx.com>
2025-03-18 23:42:15 +0200

    [-] fix next transition function in the title and credits states

--- mod/lab/state/credits/control.js
+++ mod/lab/state/credits/control.js
@@ -22,12 +22,14 @@ function onActivate() {
 function onDeactivate() {}
 
 function next() {
+    if (!this.startedAt) return
+
+    this.startedAt = 0
     trap('state/menu')
 }
 
 function evo(dt) {
     if (this.startedAt && env.time > this.startedAt + env.style.credits.keep) {
-        this.startedAt = 0
         this.next()
     }
 }
--- mod/lab/state/title/control.js
+++ mod/lab/state/title/control.js
@@ -5,12 +5,14 @@ function onActivate() {
 function onDeactivate() {}
 
 function next() {
+    if (!this.startedAt) return
+
+    this.startedAt = 0
     trap('state/menu')
 }
 
 function evo(dt) {
     if (this.startedAt && env.time > this.startedAt + env.tune.title.hold) {
-        this.startedAt = 0
         this.next()
     }
 }
